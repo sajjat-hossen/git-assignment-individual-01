725529e34c222faf94683ae619841e4ff96403de
Md. Sajjat Hossen <sajjat.hossen@brainstation-23.com>
2024-04-25 13:06:32 +0600

    Added factorial.cpp file in the feature-branch

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int n; cin >> n;
+  int fact = 1;
+  for (int i = 1; i <= n; ++i) {
+    fact *= i;
+  }
+  cout << fact << '\n';
+  return 0;
+}
